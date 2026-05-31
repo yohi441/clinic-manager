57700663c0990e626adb655aad10061ae16aa0d8
yohi441 <rikou92991@gmail.com>
2026-05-31 16:21:42 +0800

    revert: restore old inline SVG icons, remove static logo files
    Keep: login footer OptiCare v1.0.0, loading text 'Loading application…'

--- conf/settings.py
+++ conf/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_URL = '/accounts/login/'
